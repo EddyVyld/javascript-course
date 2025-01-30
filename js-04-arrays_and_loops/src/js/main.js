9a0d24fbe42675c130991343ab10c91633f07d38
Eddy <edgar.vilchis@politicas.unam.mx>
2025-01-30 17:19:34 -0600

    Feat: Break y Label

--- js-04-arrays_and_loops/src/js/main.js
+++ js-04-arrays_and_loops/src/js/main.js
@@ -72,4 +72,82 @@ refGeletinasLista.innerHTML = gelatinasLista.join("");
 
 //InnerText me lo pasa como texto
 //InnerHTML para etiquetas 
-// Me va a salir una coma porque cuando convertimos string los array me salen con coma, para quitarlo hay que que poner .join
+// Me va a salir una coma porque cuando convertimos string los array me salen con coma, para quitarlo hay que que poner .join, este me ayuda a saber que tipo de espacio voy a dejar
+
+
+// Simplificaciones en la iteración de un arreglo
+// ----------------------- Uso de for... of ------------------
+/* Ejecuta una sentencia por cada elemento de un objeto iterable(array, colección, string).
+Sintaxis:
+    for (const iterator of object) {
+    
+    }
+*/
+
+
+const colores = ["Rojo", "Azul", "Verde", "Amarillo"];
+
+for (let i = 0; i < colores.length; i++) {
+    const element = colores[i];
+    console.log(element);    
+}
+
+for ( const color of colores ) {
+    console.log( color );
+}
+
+// ---------------------- Ejercicio con for of ---------------------
+/**
+ *  Del siguiente arreglo de cantantes, mostrar en el DOM, el listado como unorder list.
+ *  const cantantes = ["Juan Gabriel", "José José", "Rocío Dúrcal", "Ana Gabriel"];
+ *  - Usar for of
+ *  - De preferencia usar una función 
+ */
+
+
+const cantantes = ["Juan Gabriel", "José José", "Rocío Dúrcal", "Ana Gabriel"];
+const refListaCantantes = document.getElementById("cantantes-lista");
+
+for ( const cantante of cantantes ) {
+    console.log( cantante );
+}
+
+const generarListaCantantes = (cantantesArray) => {
+    const cantantesLista = [];
+    for (const cantante of cantantesArray) {
+        cantantesLista.push(`<li>${cantante}</li>`);
+    }
+    return cantantesLista.join("");
+}
+
+const insertarListaDeCantantesAlDOM = (lista, refDom)=> refDom.innerHTML = lista;
+
+const listItemsOfCantantes = generarListaCantantes(cantantes);
+insertarListaDeCantantesAlDOM( listItemsOfCantantes, refListaCantantes);
+
+// ------------------- Uso de break en ciclos ----------------------------
+// break detiene la ejecución de la iteración en curso y termina el ciclo.
+
+let iteracion = 0;
+
+for ( ;   ;  ){
+    console.log( `Núm de iteración: ${iteracion}` );
+    iteracion++;
+    if ( iteracion === 5 ){
+        break;
+    }
+}
+
+
+// Uso de break y label en ciclos anidados
+
+multiplicando:
+for (let i = 1; i <= 7; i++ ){
+    multiplicador:
+    for (let j = 1; j <= 10 ; j++){        
+        console.log(`${i} x ${j} = ${i * j}`);             
+        if( i >= 4 ) break multiplicando;
+    }
+}
+
+
